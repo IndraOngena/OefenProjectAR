2e61449881bddddb11291c14de8311aa92f05ea7
Indra <indra.ongena@student.vives.be>
2020-12-02 11:21:13 +0100

    Een quit button toegevoegd

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class QuitApplication : MonoBehaviour
+{
+    public ARSession session;
+
+    public void Quit()
+    {
+        if (session != null)
+        {
+            StartCoroutine(DoPreperationForQuit());
+            Application.Quit();
+        }
+    }
+
+    IEnumerator DoPreperationForQuit()
+    {
+        Destroy(session.gameObject);
+        yield return null;
+    }
+}
